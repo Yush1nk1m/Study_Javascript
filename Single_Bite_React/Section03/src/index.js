c3bd2b3c2df9df9713c7e695418aea90b15a130d
Yush1nk1m <kys010306@sogang.ac.kr>
2024-08-01 18:33:14 +0900

    20240801: Section 3 마무리

---
+++ Single_Bite_React/Section03/src/index.js
@@ -0,0 +1,10 @@
+// import m from "./math.js";
+// import { add, sub } from "./math.js";
+
+// console.log('add(1, 2), sub(1, 2): ', add(1, 2), sub(1, 2));
+// console.log('m(1, 2): ', m(1, 2));
+
+import randomColor from "randomcolor";
+
+const color = randomColor();
+console.log('color: ', color);
