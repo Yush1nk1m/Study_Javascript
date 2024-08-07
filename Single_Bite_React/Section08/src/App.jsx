d4d24a144c304f18757e020ef324ea84617dad49
Yush1nk1m <kys010306@sogang.ac.kr>
2024-08-07 18:26:09 +0900

    20240807: Section 8.3

--- Single_Bite_React/Section08/src/App.jsx
+++ Single_Bite_React/Section08/src/App.jsx
@@ -1,9 +1,33 @@
+import { useState } from "react";
 import "./App.css";
 import Editor from "./components/Editor";
 import Header from "./components/Header";
 import List from "./components/List";
 
+const mockData = [
+  {
+    id: 1,
+    isDone: false,
+    content: "React 공부하기",
+    date: new Date().getTime(),
+  },
+  {
+    id: 2,
+    isDone: true,
+    content: "NestJS 프로젝트하기",
+    date: new Date().getTime(),
+  },
+  {
+    id: 3,
+    isDone: false,
+    content: "코딩테스트 준비하기",
+    date: new Date().getTime(),
+  },
+];
+
 function App() {
+  const [todos, setTodos] = useState(mockData);
+
   return (
     <div className="App">
       <Header />
