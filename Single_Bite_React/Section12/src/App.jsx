9c678f1c12df2139a8c6a29349af5ce84f335a94
Yush1nk1m <kys010306@sogang.ac.kr>
2024-08-11 22:37:25 +0900

    20240811: Section 12.11

--- Single_Bite_React/Section12/src/App.jsx
+++ Single_Bite_React/Section12/src/App.jsx
@@ -10,15 +10,21 @@ import { createContext, useReducer, useRef } from "react";
 const mockData = [
   {
     id: 1,
-    createdDate: new Date().getTime(),
+    createdDate: new Date("2024-08-11").getTime(),
     emotionId: 1,
     content: "1번 일기 내용",
   },
   {
     id: 2,
-    createdDate: new Date().getTime(),
+    createdDate: new Date("2024-08-10").getTime(),
     emotionId: 2,
-    content: "1번 일기 내용",
+    content: "2번 일기 내용",
+  },
+  {
+    id: 3,
+    createdDate: new Date("2024-07-29").getTime(),
+    emotionId: 3,
+    content: "3번 일기 내용",
   },
 ];
 
@@ -37,8 +43,8 @@ const reducer = (state, action) => {
   }
 };
 
-const DiaryStateContext = createContext();
-const DiaryDispatchContext = createContext();
+export const DiaryStateContext = createContext();
+export const DiaryDispatchContext = createContext();
 
 function App() {
   const [data, dispatch] = useReducer(reducer, mockData);
