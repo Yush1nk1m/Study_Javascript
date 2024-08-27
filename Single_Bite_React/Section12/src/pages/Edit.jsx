8ad4c79eb856bdffcaaf21ede7fd2bd26596e154
Yush1nk1m <kys010306@sogang.ac.kr>
2024-08-27 19:06:23 +0900

    20240827: Section 12 마무리, 완강

--- Single_Bite_React/Section12/src/pages/Edit.jsx
+++ Single_Bite_React/Section12/src/pages/Edit.jsx
@@ -5,12 +5,14 @@ import Editor from "../components/Editor";
 import { useContext } from "react";
 import { DiaryDispatchContext } from "../App";
 import useDiary from "../hooks/useDiary";
+import usePageTitle from "../hooks/usePageTitle";
 
 const Edit = () => {
   const params = useParams();
   const nav = useNavigate();
   const { onDelete, onUpdate } = useContext(DiaryDispatchContext);
   const currentDiaryItem = useDiary(params.id);
+  usePageTitle(`${params.id}번 일기 수정`);
 
   const onClickDelete = () => {
     if (window.confirm("일기를 정말 삭제하시겠습니까? 복구할 수 없습니다.")) {
